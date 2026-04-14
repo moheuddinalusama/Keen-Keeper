9ebc8f494a711bfb6efc7e3545f355bebf11b313
moheuddinalusama <moheuddinalusama@gmail.com>
2026-04-14 13:10:52 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="">
+        <h1 className='text-6xl text-center  text-blue-600 font-bold '>KeenKeeper</h1>
+
+      </div>
+    </>
+  )
+}
+
+export default App
